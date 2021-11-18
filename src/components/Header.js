a5e538723de51584cc0b38e7a23bb3d35a51b265
anilinec <83233804+anilinec@users.noreply.github.com>
2021-11-18 15:51:11 -0800

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <div className="app-header">
-            React Weather Application
+            Current Weather
         </div>
     )
 }
